a58a1bc1526ebd7ce7aa12ab01a9ea5942283efb
francocarballar <francocarballar@gmail.com>
2022-07-05 20:08:59 -0300

    Agregué una descripción a la página de inicio

--- pages/_app.js
+++ pages/_app.js
@@ -8,6 +8,11 @@ function MyApp ({ Component, pageProps }) {
   return (
     <>
       <Head>
+        <link
+          rel='icon'
+          href='/media/icon/favicon.ico'
+          onload="this.rel='stylesheet'"
+        />
         <meta charset='UTF-8' />
         <meta http-equiv='X-UA-Compatible' content='IE=edge' />
         <meta name='viewport' content='width=device-width, initial-scale=1.0' />
