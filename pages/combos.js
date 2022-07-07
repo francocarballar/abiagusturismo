6e21a93d46f294ac37bdee7300366d975659579f
francocarballar <francocarballar@gmail.com>
2022-07-07 01:14:28 -0300

    Terminé la página de los Combos

--- pages/combos.js
+++ pages/combos.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Combos.module.css'
 import { TitleContainer } from '../src/components/TitleContainer'
 
@@ -16,54 +15,44 @@ export default function Combos () {
       </Head>
       <main className={styles.main}>
         <TitleContainer title='COMBOS' />
-        <figure className={styles.figure}>
-          <Image
-            src='/media/combos/Combo-1.png'
-            alt='Combo número uno de las excursiones en Mendoza'
-            width={300}
-            height={300}
-          />
-        </figure>
-        <figure className={styles.figure}>
-          <Image
-            src='/media/combos/Combo-2.png'
-            alt='Combo número dos de las excursiones en Mendoza'
-            width={300}
-            height={300}
-          />
-        </figure>
-        <figure className={styles.figure}>
-          <Image
-            src='/media/combos/Combo-3.png'
-            alt='Combo número tres de las excursiones en Mendoza'
-            width={300}
-            height={300}
-          />
-        </figure>
-        <figure className={styles.figure}>
-          <Image
-            src='/media/combos/Combo-4.png'
-            alt='Combo número cuatro de las excursiones en Mendoza'
-            width={300}
-            height={300}
-          />
-        </figure>
-        <figure className={styles.figure}>
-          <Image
-            src='/media/combos/Combo-5.png'
-            alt='Combo número cinco de las excursiones en Mendoza'
-            width={300}
-            height={300}
-          />
-        </figure>
-        <figure className={styles.figure}>
-          <Image
-            src='/media/combos/Combo-6.png'
-            alt='Combo número seis de las excursiones en Mendoza'
-            width={300}
-            height={300}
-          />
-        </figure>
+        <section className={styles.container_combos}>
+          <figure className={styles.figure}>
+            <img
+              src='/media/combos/Combo-1.png'
+              alt='Combo número uno de las excursiones en Mendoza'
+            />
+          </figure>
+          <figure className={styles.figure}>
+            <img
+              src='/media/combos/Combo-2.png'
+              alt='Combo número dos de las excursiones en Mendoza'
+            />
+          </figure>
+          <figure className={styles.figure}>
+            <img
+              src='/media/combos/Combo-3.png'
+              alt='Combo número tres de las excursiones en Mendoza'
+            />
+          </figure>
+          <figure className={styles.figure}>
+            <img
+              src='/media/combos/Combo-4.png'
+              alt='Combo número cuatro de las excursiones en Mendoza'
+            />
+          </figure>
+          <figure className={styles.figure}>
+            <img
+              src='/media/combos/Combo-5.png'
+              alt='Combo número cinco de las excursiones en Mendoza'
+            />
+          </figure>
+          <figure className={styles.figure}>
+            <img
+              src='/media/combos/Combo-6.png'
+              alt='Combo número seis de las excursiones en Mendoza'
+            />
+          </figure>
+        </section>
       </main>
     </>
   )
