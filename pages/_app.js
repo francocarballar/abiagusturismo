743ea47e74349c2c8d31dba916ebddf9d56f15a9
francocarballar <francocarballar@gmail.com>
2022-07-07 14:30:35 -0300

    Arreglé las analiticas de google

--- pages/_app.js
+++ pages/_app.js
@@ -32,14 +32,14 @@ function MyApp ({ Component, pageProps }) {
         />
         <Script
           strategy='lazyOnload'
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+          src='https://www.googletagmanager.com/gtag/js?id=G-VFF6WRN7SM'
         />
         <Script strategy='lazyOnload'>
           {`
                     window.dataLayer = window.dataLayer || [];
                     function gtag(){dataLayer.push(arguments);}
                     gtag('js', new Date());
-                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                    gtag('config', 'G-VFF6WRN7SM', {
                     page_path: window.location.pathname,
                     });
                 `}
