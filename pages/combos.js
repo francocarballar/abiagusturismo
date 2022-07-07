13166fb2acf6963f087bfe7c3c27480b82f18763
francocarballar <francocarballar@gmail.com>
2022-07-07 14:26:16 -0300

    Agrgué el botón de comprar a cada combo

--- pages/combos.js
+++ pages/combos.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Head from 'next/head'
 import styles from '../styles/Combos.module.css'
 import { TitleContainer } from '../src/components/TitleContainer'
+import { ButtonMercadoPago } from '../src/components/ButtonMercadoPago'
 
 export default function Combos () {
   return (
@@ -21,36 +22,42 @@ export default function Combos () {
               src='/media/combos/Combo-1.png'
               alt='Combo número uno de las excursiones en Mendoza'
             />
+            <ButtonMercadoPago link='https://mpago.la/2iMFah3' />
           </figure>
           <figure className={styles.figure}>
             <img
               src='/media/combos/Combo-2.png'
               alt='Combo número dos de las excursiones en Mendoza'
             />
+            <ButtonMercadoPago link='https://mpago.la/1erdiEE' />
           </figure>
           <figure className={styles.figure}>
             <img
               src='/media/combos/Combo-3.png'
               alt='Combo número tres de las excursiones en Mendoza'
             />
+            <ButtonMercadoPago link='https://mpago.la/1pykU3q' />
           </figure>
           <figure className={styles.figure}>
             <img
               src='/media/combos/Combo-4.png'
               alt='Combo número cuatro de las excursiones en Mendoza'
             />
+            <ButtonMercadoPago link='https://mpago.la/2pD8xNW' />
           </figure>
           <figure className={styles.figure}>
             <img
               src='/media/combos/Combo-5.png'
               alt='Combo número cinco de las excursiones en Mendoza'
             />
+            <ButtonMercadoPago link='https://mpago.la/23pkacu' />
           </figure>
           <figure className={styles.figure}>
             <img
               src='/media/combos/Combo-6.png'
               alt='Combo número seis de las excursiones en Mendoza'
             />
+            <ButtonMercadoPago link='https://mpago.la/1GxFDbJ' />
           </figure>
         </section>
       </main>
