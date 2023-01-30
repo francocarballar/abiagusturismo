a0f369d8612d94eacbb5c0f00d1bc4c1b0c9e603
francocarballar <francocarballar@gmail.com>
2023-01-29 21:28:17 -0300

    -m

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,6 +16,38 @@ export default function Home () {
           name='description'
           content='Abiagus Turismo es una agencia de viajes que ofrece turismo local, nacional e internacional. Contratación de hoteles, transporte y excursiones en Mendoza, Argentia tales como: Alta Montaña, Bodegas, City Tour, Cacheuta, Valle de Uco, Villavicencio, Cañón del Atuel, y en San Rafael, Las Leñas y Los Reyunos'
         />
+        <link
+          rel='preconnect'
+          href='https://res.cloudinary.com/abiagusturismo/image/upload/w_320,f_auto,c_scale,q_auto,dpr_auto,e_sharpen/v1675035947/home/aconcagua_yaupjk.jpg'
+        />
+        <link
+          rel='preconnect'
+          href='https://res.cloudinary.com/abiagusturismo/image/upload/w_320,f_auto,c_scale,q_auto,dpr_auto,e_sharpen/v1675035948/home/uvas_ixpubb.jpg'
+        />
+        <link
+          rel='preconnect'
+          href='https://res.cloudinary.com/abiagusturismo/image/upload/w_375,f_auto,c_scale,q_auto,dpr_auto,e_sharpen/v1675035947/home/aconcagua_yaupjk.jpg'
+        />
+        <link
+          rel='preconnect'
+          href='https://res.cloudinary.com/abiagusturismo/image/upload/w_375,f_auto,c_scale,q_auto,dpr_auto,e_sharpen/v1675035948/home/uvas_ixpubb.jpg'
+        />
+        <link
+          rel='preconnect'
+          href='https://res.cloudinary.com/abiagusturismo/image/upload/w_425,f_auto,c_scale,q_auto,dpr_auto,e_sharpen/v1675035947/home/aconcagua_yaupjk.jpg'
+        />
+        <link
+          rel='preconnect'
+          href='https://res.cloudinary.com/abiagusturismo/image/upload/w_425,f_auto,c_scale,q_auto,dpr_auto,e_sharpen/v1675035948/home/uvas_ixpubb.jpg'
+        />
+        <link
+          rel='preconnect'
+          href='https://res.cloudinary.com/abiagusturismo/image/upload/w_505,f_auto,c_scale,q_auto,dpr_auto,e_sharpen/v1675035947/home/aconcagua_yaupjk.jpg'
+        />
+        <link
+          rel='preconnect'
+          href='https://res.cloudinary.com/abiagusturismo/image/upload/w_505,f_auto,c_scale,q_auto,dpr_auto,e_sharpen/v1675035948/home/uvas_ixpubb.jpg'
+        />
       </Head>
       <main>
         <section className={styles.section__main}>
